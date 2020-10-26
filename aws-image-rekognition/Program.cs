0f7b2846159ed1e05fc3f1d371f85f7865b3bb06
Alex Bierhaus <abierhaus@compeon.de>
2020-10-26 10:30:15 +0100

    Add project files.

---
+++ aws-image-rekognition/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.Rekognition;
+using Amazon.Rekognition.Model;
+
+namespace aws_image_rekognition
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            //Set Logging level
+
+            AWSConfigs.LoggingConfig.LogTo = LoggingOptions.Console;
+            AWSConfigs.LoggingConfig.LogMetrics = true;
+            AWSConfigs.LoggingConfig.LogResponses = ResponseLoggingOption.OnError;
+
+            await Detect();
+        }
+
+        public static async Task Detect()
+        {
+            //Get picture from local storage
+            var photo = "img/sheep.png";
+
+            var image = new Image();
+            try
+            {
+                await using var fs = new FileStream(photo, FileMode.Open, FileAccess.Read);
+                byte[] data = null;
+                data = new byte[fs.Length];
+                fs.Read(data, 0, (int)fs.Length);
+                image.Bytes = new MemoryStream(data);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Failed to load file " + photo);
+                return;
+            }
+
+            var rekognitionClient = new AmazonRekognitionClient();
+
+            //Set settings for label detection
+            var detectlabelsRequest = new DetectLabelsRequest
+            {
+                Image = image,
+                MaxLabels = 3,
+                MinConfidence = 77F
+            };
+
+
+            var detectLabelsResponse = await rekognitionClient.DetectLabelsAsync(detectlabelsRequest);
+            Console.WriteLine("Detected labels for " + photo);
+            foreach (var label in detectLabelsResponse.Labels)
+                Console.WriteLine("{0}: {1}", label.Name, label.Confidence);
+        }
+    }
+}
---
+++ aws-image-rekognition/README.md
@@ -0,0 +1,16 @@
+﻿# AWS Image Rekognition Example
+
+This console application demonstrate how to use the AWS Image Rekognition in C# (.NET Core). Since I did not find a working example, I created one myself.
+The example comes with some animal pictures that can be used for testing the labels. This example comes without the need for any lambda or configuration within your AWS account. Just download and run it.
+
+
+## Getting started
+
+1. Make sure you configured your IAM User and your local environment like described in https://docs.aws.amazon.com/rekognition/latest/dg/images-bytes.html
+2. Run the sample
+
+
+## Other useful resources
+
+* Some more (outdated) examples can be found https://docs.aws.amazon.com/rekognition/latest/dg/labels-detect-labels-image.html. 
+  
